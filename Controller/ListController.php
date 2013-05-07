55713e107b218de08a3a123e5ecfa85fcb02450c
Jarrett Croll <jarrett.croll@gmail.com>
2013-05-07 11:04:55 -0700

    more work on sidebar view

--- Controller/ListController.php
+++ Controller/ListController.php
@@ -3,26 +3,28 @@
 namespace Vespolina\TaxonomyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Vespolina\Entity\Taxonomy\TaxonomyNode;
+use Vespolina\Entity\Taxonomy\TaxonomyNodeInterface;
 
 class ListController extends Controller
 {
 
-    public function listAction($taxonomyNodes, $routeName, array $routeParams = null)
+    public function listAction($taxonomyNodes, $routeName, $taxonomyIdParam = 'id', array $routeParams = null)
     {
+        $routes = array();
         foreach ($taxonomyNodes as $taxonomyNode) {
-            /** @var $taxonomyNode TaxonomyNode */
-
+            $routes[] = $this->createRoute($taxonomyNode, $routeName, $taxonomyIdParam, $routeParams);
         }
 
         return $this->renderView('VespolinaTaxonomyBundle:Render:renderTaxonomyNodes.html.twig', array(
             'taxonomyNodes' => $taxonomyNodes,
-
+            'routes' => $routes
         ));
     }
 
-    protected function createRoute()
+    protected function createRoute(TaxonomyNodeInterface $taxonomyNode, $routeName, $taxonomyIdParam, array $routeParams)
     {
-
+        return $this->generateUrl($routeName, array_merge($routeParams, array(
+            $taxonomyIdParam => $taxonomyNode->getId()
+        )));
     }
 }
